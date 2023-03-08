351f937ef52978a59929f7d9e5dc0ad8c5d29403
AliyevMura <7lvghl9@code.edu.az>
2023-03-08 14:11:33 +0400

    Initial commit

---
+++ ArraysTask2/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+
+//Task1
+//int[] arr = new int[] { 1, 2, 3, 4, 5 };
+//int sum = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum = sum + arr[i];
+//}
+//Console.WriteLine("arrayin butun elementlerinin cemi: " + sum);
+
+
+
+
+//Task2
+//string[] adlar = { "heyva", "alma", "banan" };
+//for(int i = 0; i< adlar.Length; i++)
+//{
+//    if (adlar[i].Length > 4)
+//    {
+//        Console.WriteLine(adlar[i]);
+//    }
+//}
+
+
+
+//Task3
+//int[] arr = { 2, 3, 7, 45, };
+//int
+//for (int i =0; i < arr.Length; i++)
+//{
+//    if (arr[i] <= 10)
+//    {
+//        Console.WriteLine(arr[i]);
+
+//    }
+//}
+//Task 4
+//static void Main(string[] args)
+//{
+//    int[] arr1 = { 12, 56, 76, 98, 43 };
+//    int[] arr2 = { 23, 45, 56, 77, 43 };
+//    for (int i = 0; i < arr1.Length; i++)
+//    {
+//        for (int j = 0; i < arr2.Length; j++)
+//        {
+//            if (arr1[i] == arr2[j])
+//            {
+//                Console.WriteLine(arr1[i]);
+//            }
+//        }
+
+//    }
+//}
